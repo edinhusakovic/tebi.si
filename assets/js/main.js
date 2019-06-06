9def701273e9f819dff9921c294feab28298ac67
Edin Husakovic <eh7842@student.uni-lj.si>
2019-06-06 23:45:24 +0200

    Shop narejen

--- assets/js/main.js
+++ assets/js/main.js
@@ -648,14 +648,14 @@
         sliderrange.slider({
             range: true,
             min: 20,
-            max: 100,
-            values: [0, 100],
+            max: 1000,
+            values: [0, 1000],
             slide: function(event, ui) {
-                amountprice.val("$" + ui.values[0] + " - $" + ui.values[1]);
+                amountprice.val("€" + ui.values[0] + " - €" + ui.values[1]);
             }
         });
-        amountprice.val("$" + sliderrange.slider("values", 0) +
-            " - $" + sliderrange.slider("values", 1));
+        amountprice.val("€" + sliderrange.slider("values", 0) +
+            " - €" + sliderrange.slider("values", 1));
     });
     
     /*--------------------------
